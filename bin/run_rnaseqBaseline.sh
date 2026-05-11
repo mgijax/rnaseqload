3abcef989567e725787961434b56b27cec9f4124
Lori Corbani <lori.corbani@jax.org>
2026-05-11 12:21:37 -0400

    wts2-1854/Baseline RNA-Seq Load

---
+++ bin/run_rnaseqBaseline.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+#
+# Purpose:
+#	Run Baselinne Processing
+#
+
+cd `dirname $0`
+
+CONFIG_LOAD=../rnaseqload.config
+
+#
+# Make sure the common configuration file exists and source it.
+#
+if [ -f ${CONFIG_LOAD} ]
+then
+    . ${CONFIG_LOAD}
+else
+    echo "Missing configuration file: ${CONFIG_LOAD}"
+    exit 1
+fi
+
+echo "Baseline Processing" 
+rm -rf ${BASELINELOG}
+${PYTHON} ${RNASEQLOAD}/bin/rnaseqBaselineload.py >> ${BASELINELOG} 2>&1
+STAT=$?
+
+date | tee -a ${BASELINELOG_DOWNLOAD}
