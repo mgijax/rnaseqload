29c4bef4823b5d36e25d1b2b8dbba8611906241c
Lori Corbani <lori.corbani@jax.org>
2026-05-12 07:53:19 -0400

    wts2-1854/Baseline RNA-Seq Load

--- bin/run_setbaseline.sh
+++ bin/run_setbaseline.sh
@@ -1,6 +1,11 @@
 #
 #  Purpose:
-#	quick script to run the baseline setload
+#	refresh:
+#		mgi_set
+#		mgi_setmember
+#		gxd_htsample_rnaseqset
+#		gxd_htsample_rnaseqsetmember
+#		gxd_htsample_rnaseqset_cache
 #
 
 if ( ${?MGICONFIG} == 0 ) then
@@ -16,12 +21,19 @@ setenv LOG $0.log
 rm -rf $LOG
 touch $LOG
 
-echo "Run baseline setload" 
-${SETLOAD}/setload.csh ${RNASEQLOAD}/baselinesetload.config
+echo "Run baseline setload" | tee -a $LOG
+${SETLOAD}/setload.csh ${RNASEQLOAD}/baselinesetload.config | tee -a $LOG
+
+echo "Reload RNASet/SetMember & RNASeqSet_Cache All MGI_Sets" | tee -a $LOG
+${RNASEQLOAD}/bin/reloadBioRepsAndCache.sh | tee -a $LOG
 
 cat - <<EOSQL | ${PG_DBUTILS}/bin/doisql.csh $0 | tee -a $LOG
+
 select s.* from MGI_Set s where s.name = 'Baseline RNASeq Load Experiment';
-select a.accid, m.* from MGI_Set s, MGI_SetMember m , ACC_Accession a
+
+select a.accid, m.* 
+into temp table baseline
+from MGI_Set s, MGI_SetMember m , ACC_Accession a
 where s.name = 'Baseline RNASeq Load Experiment'
 and s._set_key = m._set_key
 and s._mgitype_key = a._mgitype_key
@@ -29,6 +41,16 @@ and m._object_key = a._object_key
 and a._logicaldb_key = 189
 and a.preferred = 1
 ;
-EOSQL
 
+select * from baseline;
+
+select b.*, s.*, m.*
+from baseline b, GXD_HTSample_RNASeqSet s, GXD_HTSample_RNASeqSetMember m
+where b._object_key = s._experiment_key
+and s._rnaseqset_key = m._rnaseqset_key
+;
+
+--select * from GXD_HTSample_RNASeqSet_Cache limit 1;
+
+EOSQL
 
