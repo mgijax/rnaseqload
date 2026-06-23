f9627f12e7d733eb45a0c3f37472583362160330
Lori Corbani <lori.corbani@jax.org>
2026-06-23 11:22:29 -0400

    wts2-1854/Baseline RNA-Seq Load

--- bin/rnaseqBaselineDelete.sh
+++ bin/rnaseqBaselineDelete.sh
@@ -18,31 +18,6 @@ touch ${BASELINELOG}
 
 cat - <<EOSQL | ${PG_DBUTILS}/bin/doisql.csh $0 >> ${BASELINELOG} 2>&1
 
--- start: delete _CreatedBy_key = 1613 for the baseline experiments
--- this only needs to be run once & can then be removed
-
-select s.*
-into temp toDelete
-from GXD_HTSample_RNASeqSet s, MGI_SetMember sm
-where s._createdBy_key = 1613
-and s._experiment_key = sm._object_key
-and sm._set_key = 1061
-;
-
-create index idx1 on toDelete (_rnaseqset_key);
-
-delete from GXD_HTSample_RNASeqCombined s
-using toDelete d
-where d._rnaseqset_key = s._rnaseqset_key
-;
-
-delete from GXD_HTSample_RNASeqSet s
-using toDelete d
-where d._rnaseqset_key = s._rnaseqset_key
-;
-
--- end: delete _CreatedBy_key = 1613 for the baseline experiments
-
 delete from GXD_HTSample_RNASeqCombined where _createdBy_key = 1673;
 delete from GXD_HTSample_RNASeqSet where _createdBy_key = 1673;
 
